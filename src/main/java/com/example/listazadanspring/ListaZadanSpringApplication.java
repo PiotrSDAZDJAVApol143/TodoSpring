96fa1a06bebd06b4fc93f20e83b5cdb1730634c2
PiotrSDAZDJAVApol143 <146108817+PiotrSDAZDJAVApol143@users.noreply.github.com>
2024-01-27 18:59:57 +0100

    Start pojektu

---
+++ src/main/java/com/example/listazadanspring/ListaZadanSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.listazadanspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaZadanSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListaZadanSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/listazadanspring/ListaZadanSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listazadanspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaZadanSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
