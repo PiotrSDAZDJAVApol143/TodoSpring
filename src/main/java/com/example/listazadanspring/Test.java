653e7a846750077e2c3e213b17a26d6f32b5c72b
PiotrSDAZDJAVApol143 <xav88er@gmail.com>
2024-01-28 14:19:32 +0100

    first commit

--- src/main/java/com/example/listazadanspring/Test.java
+++
@@ -1,2 +0,0 @@
-package com.example.listazadanspring;public class Test {
-}
---
+++ src/main/java/com/example/listazadanspring/model/Task.java
@@ -0,0 +1,8 @@
+package com.example.listazadanspring;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Test {
+    //test
+}
