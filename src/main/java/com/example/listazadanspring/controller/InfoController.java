c19a1db4cd9dd830b929262fc9adbbf67ecb14ae
PiotrSDAZDJAVApol143 <xav88er@gmail.com>
2024-02-07 23:17:53 +0100

    profile yml

--- src/main/java/com/example/listazadanspring/controller/InfoController.java
+++ src/main/java/com/example/listazadanspring/controller/InfoController.java
@@ -1,14 +1,17 @@
 package com.example.listazadanspring.controller;
 
+import com.example.listazadanspring.configuration.TaskConfiguration;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.RestController;
-
+@AllArgsConstructor
 @RestController("/info")
 public class InfoController {
-    @Autowired
     private DataSourceProperties dataSourceProperties;
-@Value("${task.allowMultipleTasksFromTemplate")
-    private String myProp;
+    private TaskConfiguration taskConfiguration;
+    boolean taskConfiguration(){
+        return taskConfiguration.isAllowMultipleTasksFromTemplate();
+    }
 }
