792d682c9655406ed06b362b22ad8fd420b8c635
PiotrSDAZDJAVApol143 <xav88er@gmail.com>
2024-02-10 00:27:36 +0100

    working migration!

--- src/main/java/com/example/listazadanspring/controller/InfoController.java
+++ src/main/java/com/example/listazadanspring/controller/InfoController.java
@@ -2,8 +2,6 @@ package com.example.listazadanspring.controller;
 
 import com.example.listazadanspring.configuration.TaskConfiguration;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
---
+++ src/main/resources/db/migration/V1__init_task_table.sql
@@ -0,0 +1,12 @@
+drop table if exists tasks;
+
+create table tasks(
+
+                      id INT PRIMARY KEY AUTO_INCREMENT,
+
+                      description VARCHAR(100) NOT NULL,
+
+                      done_check bit
+
+);
+
